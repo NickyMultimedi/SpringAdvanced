46132546654b37f94ee632d995d589e6a5a43010
Nick Bauters <nick.bauters@multimedi.be>
2019-08-14 09:46:22 +0200

    Opdracht 1, de initialisatie van het project

---
+++ src/main/java/be/multimedi/springAdvanced/BeerApp.java
@@ -0,0 +1,11 @@
+package be.multimedi.springAdvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BeerApp.class, args);
+    }
+}
